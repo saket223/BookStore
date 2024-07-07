b9b4a82a4ef0b9a75049589aace1276b6704824f
saket223 <arora.saket20@gmail.com>
2024-07-08 00:40:48 +0530

    Step 5: Authorization

--- angular/src/app/book/book-routing.module.ts
+++ angular/src/app/book/book-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { authGuard, permissionGuard } from '@abp/ng.core';
 import { BookComponent } from './book.component';
 
-const routes: Routes = [{ path: '', component: BookComponent }];
+const routes: Routes = [
+  { path: '', component: BookComponent, canActivate: [authGuard, permissionGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class BookRoutingModule { }
+export class BookRoutingModule {}
--- aspnet-core/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ aspnet-core/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -4,13 +4,11 @@ public static class BookStorePermissions
 {
     public const string GroupName = "BookStore";
 
-    public static class Dashboard
+    public static class Books
     {
-        public const string DashboardGroup = GroupName + ".Dashboard";
-        public const string Host = DashboardGroup + ".Host";
-        public const string Tenant = DashboardGroup + ".Tenant";
+        public const string Default = GroupName + ".Books";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
     }
-
-    //Add your own permission names. Example:
-    //public const string MyPermission1 = GroupName + ".MyPermission1";
 }
